d77099aafd037b6b77ee2bac78590eefd04a678f
Dinosaur-debug <68700904+Dinosaur-debug@users.noreply.github.com>
2020-08-05 21:00:44 -0500

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,3 +1,6 @@
+const request = require('request')
+const chalk = require('chalk')
+
 const forecast = (address, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=a1493d28f272683a47edcbac34c338aa&query='+ address +''
 
