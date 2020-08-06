64db45fadc5a224d8d677a7e1a82082b286190c3
Dinosaur-debug <68700904+Dinosaur-debug@users.noreply.github.com>
2020-08-05 20:59:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
 const chalk = require('chalk');
 const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'http://api.weatherstack.com/current?access_key=a1493d28f272683a47edcbac34c338aa&query=Guayaquil'
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log(chalk.inverse.redBright('Unable to connect to weather service!'))
-
-    } else if (response.body.error) {
-        console.log(chalk.inverse.redBright('Unable to find location of weather service!'))
-
-    } else {
-        const data = response.body.current
-        console.log(data.weather_descriptions[0] + '. It is curently ' + data.temperature + ' °C degress out. There is a ' + data.precip + '% chance of rain' )
-
-    }
+geocode('Name of your city', (error, data) => {
+    console.log(chalk.redBright('Error:'), error)
+    console.log(chalk.greenBright('Data:'), data)
 })
 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Guayaquil.json?access_token=pk.eyJ1IjoiZGlub3NhdXJpb2pzIiwiYSI6ImNrZGM5ZW9hYTQyN2UydXF5dzBwcmtjdDQifQ.YgWQUHqHjRgnSE0dOuR9XA&limit=1'
- 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    const location = response.body.current
-
-    if (error) {
-        console.log(chalk.inverse.redBright('Unable to connect to geocoding service!'))
-
-    } else if (!location) {
-        console.log(chalk.inverse.redBright('Unable to find location of geocoding service!'))
-
-    } else {
-        const lat = response.body.features[0].center[1]
-        const long = response.body.features[0].center[0]
-        console.log(lat, long)
-
-    }
-
-}) 
+forecast('Name of you city', (error, data) => {
+  console.log(chalk.redBright('Error:'), error)
+  console.log(chalk.greenBright('Data:'), data)
+})
