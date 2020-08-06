52ab9f7da3da3162ca3aba91f1f1e41b57286fe9
Dinosaur-debug <68700904+Dinosaur-debug@users.noreply.github.com>
2020-08-06 17:24:42 -0500

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,19 +1,19 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (address, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=a1493d28f272683a47edcbac34c338aa&query='+ address +''
+    const url = 'http://api.weatherstack.com/current?access_key=a1493d28f272683a47edcbac34c338aa&query='+ address +'';
 
-     request({ url: url, json: true }, (error, response) => {
+     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Unable to connect to weather service!', undefined)
+            callback('Unable to connect to weather service!', undefined);
 
-        } else if (response.body.error) {
-            callback('Unable to find location. Try another search.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined);
 
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + '. It is curently ' + response.body.current.temperature + ' °C degress out. There is a ' + response.body.current.precip + '% chance of rain')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' °C degress out. There is a ' + body.current.precip + '% chance of rain');
         }
      })
 }  
 
-module.exports = forecast
+module.exports = forecast;
